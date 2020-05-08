519529ab8a656b68d562129496447da54378f214
Dika Haris Abdurahman <dika@jsc.co.id>
2020-05-09 06:55:37 +0700

    membuat about dan title all page

---
+++ src/routes/about.svelte
@@ -0,0 +1,12 @@
+<svelte:head>
+    <title>About</title>
+</svelte:head>
+
+<div class="section header">
+    <div class="container">
+        <h1>About</h1>
+        <p>
+            Data yang ada di aplikasi ini bersumber dari <a href="https://covid19-public.digitalservice.id/api/v1/">API Publik PIKOBAR</a>
+        </p>
+    </div>
+</div>
